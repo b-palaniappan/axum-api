89e039a553bab5d88c18a30c4be92a1ca579e519
Balasubramanian Palaniappan <bpalaniappan@jobcase.com>
2023-03-19 13:27:21 -0500

    working example for sqlx

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use axum::{
   response::{Html, IntoResponse},
   Router, routing::get,
 };
+use axum::extract::State;
 use chrono::{SecondsFormat, Utc};
 use serde::{Deserialize, Serialize};
+use sqlx::{Error, MySqlPool};
+use sqlx::mysql::MySqlPoolOptions;
 use tracing::{error, info};
 
 pub mod api {
@@ -19,10 +22,24 @@ async fn main() {
   // Logging handler using tracing.
   tracing_subscriber::fmt().init();
 
+  // Get Database connection string from ENV variable.
+  let db_connection_str = std::env::var("DATABASE.URL").unwrap_or_else(|_| "mysql://localhost".to_string());
+
+  // Setup connection pool.
+  let pool = MySqlPoolOptions::new()
+    .max_connections(10)
+    .connect(&db_connection_str)
+    .await
+    .map_err(|e| {
+      error!("Failed to create database connection pool: {}", e);
+      panic!("Failed to create database connection pool: {}", e);
+    }).unwrap();
+
   // build our application with a route
   let app = Router::new()
     .route("/", get(handler))
     .route("/json", get(handler_json))
+    .with_state(pool)
     .nest("/hello", api::handlers::hello_handler::routes())
     .nest("/users", api::handlers::users_handler::routes());
 
@@ -49,8 +66,17 @@ async fn handler() -> Html<&'static str> {
 }
 
 // JSON handlers
-async fn handler_json() -> impl IntoResponse {
+async fn handler_json(State(pool): State<MySqlPool>) -> impl IntoResponse {
   info!("Handle Json payload");
+
+  // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
+  let response: Result<(String, ), Error> = sqlx::query_as("SELECT 'Hello'")
+    .fetch_one(&pool).await;
+  match response {
+    Ok(r) => info!("DB Response -> {}", r.0),
+    Err(e) => error!("Error getting data {}", e),
+  }
+
   Json(Message {
     message: "Hello".to_string(),
     status: "Success".to_string(),
